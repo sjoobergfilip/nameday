7dff1d76eed01dda6fa8cb43736b457a0fafe56a
Filip Sjöberg <filip.sjoberg@hotmail.com>
2020-01-16 15:57:49 +0100

    added a error msg if not name have nameday

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,4 +1,12 @@
 
+//if name not found
+const renderNotFound = (msg => {
+    document.querySelector('#namedays').innerHTML = `<p>${msg}</p>
+    `;
+});
+
+
+
 //Get todyas nameday when page load
 window.addEventListener('load', (e) => {
 
@@ -7,26 +15,26 @@ window.addEventListener('load', (e) => {
     getTodaysNameDay(contryEl).then(data =>{
         const todayNameDay = document.querySelector('#today-nameday')
         //get todays date
-        const today = moment().format("DD MMMM");
+        const today = moment().format("DD MMM");
         //Make a template for nameday of today
         data.data.forEach(info => {
             const string = 'se';
-            todayNameDay.innerHTML = `<p>${today} har <span> ${info.namedays[string]}</span> namsdag</p>`;
+            todayNameDay.innerHTML = `<p>${today} har <span> ${info.namedays[string]}</span> namsdag!</p>`;
         });
     });
 });
 //Change contry and check today nameday
 document.querySelector('#search-form-today').addEventListener('submit', e => {
-    e.preventDefault();
+        e.preventDefault();
     const contryEl = document.querySelector('#country-form').value;
     getTodaysNameDay(contryEl).then(data =>{
         const todayNameDay = document.querySelector('#today-nameday')
         //get todays date
-        const today = moment().format("DD MMMM");
+        const today = moment().format("DD MMM");
         //Make a template for nameday of today
         data.data.forEach(info => {
             const string = contryEl;
-            todayNameDay.innerHTML = `<p>${today} har <span> ${info.namedays[string]}</span> namsdag</p>`;
+            todayNameDay.innerHTML = `<p>${today} har <span> ${info.namedays[string]}</span> namsdag!</p>`;
         });
     });
 });
@@ -38,23 +46,27 @@ document.querySelector('#search-form').addEventListener('submit', e => {
     const nameEl = document.querySelector('#name').value;
     const countEl = document.querySelector('#country').value
     const nameDaysEl = document.querySelector('#namedays');
-     if (nameEl < 2){
-        return;
-    }
 
     getNameDay(nameEl, countEl)
     .then(data =>{
-        const nameEl = document.querySelector('#name').value;
 
-        let output = ''
-        data.results.forEach((data) => {
-            output = ` <p class="search-name">Du har sökt på <span>${nameEl}</span></p>
-            <p><span>${data.name}</span> har namsdag den</p>
-            <p><span>${data.day}/${data.month}</span></p>
-            `;
-        });
-        nameDaysEl.innerHTML = output;
+        //Else if (Name not found)
+        if(200 && data.results.length > 0){
+                const nameEl = document.querySelector('#name').value;
+        
+                let output = ''
+                data.results.forEach((data) => {
+                    output = ` <p class="search-name">Du har sökt på <span>${nameEl}</span></p>
+                    <p><span>${data.name}</span> har namsdag den</p>
+                    <p><span>${data.day}/${data.month}</span></p>
+                    `;
+                });
+                nameDaysEl.innerHTML = output;
+        } else {
+            renderNotFound('Sorry, namnet du sökte fanns inte');
+        }
     });
+        
 });
 
 
